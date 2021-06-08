5312b5a92b77fc453d37a797432905b79822aea1
RodrigoTeixeiraC <rodrigo.tchagas@gmail.com>
2021-06-08 16:11:03 -0300

    Crud blog pessoal

--- Angular/blogPessoal/src/app/service/auth.service.ts
+++ Angular/blogPessoal/src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -10,9 +10,11 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin)
@@ -21,6 +23,10 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>("http://localhost:8080/usuario/cadastrar", user)
   }
+
+    getByIdUser(id: number): Observable<User>{
+      return this.http.get<User>(`http://localhost:8080/usuario/${id}`, this.token)
+    }
 
   logado(){
     let ok: boolean = false
